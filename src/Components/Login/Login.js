4de687e591757cfef6524be3f27bc8c980a38885
Kris Cooper <prymaldesign@gmail.com>
2019-12-16 19:36:38 -0700

    added register functionality

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -6,17 +6,30 @@ import axios from 'axios';
 class Login extends Component{
     constructor(props){
         super(props)
-        this.state = {}
+        this.state = {
+            inputEmail: '',
+            inputPassword: ''
+        }
+    }
+
+    updateInput = (event) => {
+
+        this.setState({
+            [event.target.name]: event.target.value
+        });
     }
 
     render(){
         return(
             <div>
                 <div className='loginInputs'>
-                    <input type='text' placeholder='email' />
-                    <input type='password' placeholder='password' />
+                    <input type='text' placeholder='email' name='inputEmail' value={this.state.inputEmail} 
+                    onChange={this.updateInput}/>
+                    <input type='password' placeholder='password' name='inputPassword' value={this.state.inputPassword}
+                    onChange={this.updateInput}/>
                     <button>Login</button>
                     <h3>Not a member? <Link to='/Register'>Register</Link> </h3>
+
                 </div>
             </div>
         )
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -6,20 +6,45 @@ import axios from 'axios';
 class Register extends Component{
     constructor(props){
         super(props)
-        this.state = {}
+        this.state = {
+            inputFirst: '',
+            inputLast: '',
+            inputEmail: '',
+            inputPassword: '',
+            inputPassword2: ''
+        }
+    }
+
+    updateInput = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+
     }
 
     render(){
         return(
             <div>
                 <div className='registerInfo'>
-                    <input type='text' placeholder='first name' />
-                    <input type='text' placeholder='last name' />
-                    <input type='text' placeholder='email' />
-                    <input type='text' placeholder='password' />
-                    <input type='text' placeholder='re-type password' />
+                    <input type='text' placeholder='first name' name='inputFirst' value={this.state.inputFirst} onChange={this.updateInput}/>
+                    <input type='text' placeholder='last name' name='inputLast' value={this.state.inputLast} onChange={this.updateInput}/>
+                    <input type='text' placeholder='email' name='inputEmail' value={this.state.inputEmail} onChange={this.updateInput}/>
+                    <input type='text' placeholder='password' name='inputPassword' value={this.state.inputPassword} onChange={this.updateInput}/>
+                    <input type='text' placeholder='re-type password' name='inputPassword2' value={this.state.inputPassword2} onChange={this.updateInput}/>
 
                     <button>Register</button>
+
+                    <h1>All the stuff:</h1>
+                    {this.state.inputFirst}
+                    <br />
+                    {this.state.inputLast}
+                    <br />
+                    {this.state.inputEmail}
+                    <br />
+                    {this.state.inputPassword}
+                    <br />
+                    {this.state.inputPassword2}
+
                     <h3>Already a member? <Link to='/Login'>Login</Link></h3>
                 </div>
             </div>
